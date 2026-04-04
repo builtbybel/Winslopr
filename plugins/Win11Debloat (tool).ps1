8e03167e9a1871efcc61a8d25c0f86c907690a92
Belim <belim@builtbybel.com>
2026-04-04 22:40:02 +0200

    Add description comment to Win11Debloat script
    Added a description comment for the script.

--- plugins/Win11Debloat (tool).ps1
+++ plugins/Win11Debloat (tool).ps1
@@ -1,1 +1,2 @@
-﻿& ([scriptblock]::Create((irm "https://debloat.raphi.re/")))
+# Description: Launches Raphire's Win11Debloat using the official remote bootstrap to remove bloatware and optimize Windows.
+ & ([scriptblock]::Create((irm "https://debloat.raphi.re/")))
